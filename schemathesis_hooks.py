1a9c1636467a83c8ab115fb210fb3d6c1d3d1ebc
Adam Kliment <adam@netmilk.net>
2024-06-28 15:57:21 +0200

    Sanitize output

--- schemathesis_hooks.py
+++ schemathesis_hooks.py
@@ -1,5 +1,15 @@
 import schemathesis
 
+# Create a custom config
+custom_config = (
+        schemathesis.sanitization.Config(replacement="[Filtered]")
+    .with_keys_to_sanitize("token")
+    .with_sensitive_markers("token")
+)
+
+# Configure Schemathesis to use your custom sanitization configuration
+schemathesis.sanitization.configure(custom_config)
+
 @schemathesis.hook
 def map_headers(context, headers):
     if headers:
