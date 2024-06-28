f691bb3d266ee9f53899eb60aab165e625c12a3c
drobnikj <drobnik.j@gmail.com>
2024-06-28 16:46:54 +0200

    fix: update

--- scripts/add_urls.py
+++ scripts/add_urls.py
@@ -47,15 +47,12 @@ for path, ref in openapi_yaml.get('paths').items():
         operationId = operation.get('operationId')
         # Some update logic
         summary = operation.get('summary')
-        
+
         summary_kabab_case = '-'.join(summary.lower().split(' '))
         # Find in mapping list by API reference URL which is equal to summary
         found_refferece = None
         for legacy_item in all_legacy_link:
             if summary in legacy_item.get('text'):
-                # Do something with the matching item
-                # For example, you can access its values using item['key']
-                # Replace the placeholder code below with your desired logic
                 print(f"Found matching item: {legacy_item.get('text')} for {summary}")
                 found_refferece = legacy_item
                 operation['x-legacy-doc-url'] = legacy_item.get('url')
@@ -65,19 +62,7 @@ for path, ref in openapi_yaml.get('paths').items():
             notfound.append(summary)
             print(f"Could not find matching item for {summary}")
             continue
-        
-        # if not found_refferece:
-        #     notfound += 1
-        #     print(f"Could not find matching item for {summary_kabab_case}")
-        #     continue
-        
+
     with open(path_path, 'w') as file:
         yaml.safe_dump(path_yaml, file, default_style=None, sort_keys=False)
-
-
-
-print(f'Not found: {len(notfound)}')
-print(notfound)
-
-print(f'Updated path: {path_path}')
 
