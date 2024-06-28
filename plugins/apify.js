090a6b391a7b35836c175b982000658b902fe518
Tobiáš Potoček <tobias.potocek@apify.com>
2024-06-28 15:47:44 +0200

    feat: add support for Tags as well

--- plugins/apify.js
+++ plugins/apify.js
@@ -1,10 +1,10 @@
 const LegacyDocUrlDecorator = require("./decorators/legacy-doc-url-decorator");
 
 module.exports = {
-  id: 'apify',
-  decorators: {
-    oas3: {
-      'legacy-doc-url-decorator': LegacyDocUrlDecorator
+    id: 'apify',
+    decorators: {
+        oas3: {
+            'legacy-doc-url-decorator': LegacyDocUrlDecorator
+        }
     }
-  }
 }
--- plugins/decorators/legacy-doc-url-decorator.js
+++ plugins/decorators/legacy-doc-url-decorator.js
@@ -1,7 +1,7 @@
 const X_LEGACY_DOC_URL_PROPERTY = "x-legacy-doc-url";
 
 /**
- * Prepends an HTML anchor to the description of each operation with an HTML fragment from the legacy documentation.
+ * Prepends an HTML anchor to the description of each node with an HTML fragment from the legacy documentation.
  * This achieves link backward compatibility.
  *
  * For example, if the old URL is https://docs.apify.com/api/v2#/reference/logs/log/get-log, then we prepend:
@@ -9,24 +9,25 @@ const X_LEGACY_DOC_URL_PROPERTY = "x-legacy-doc-url";
  *
  * The legacy URL is stored on a custom property `x-legacy-doc-url` in the operation object.
  */
-function LegacyDocUrlDecorator() {
-  return {
-    Operation: {
-      leave(operation) {
-        if (operation[X_LEGACY_DOC_URL_PROPERTY]) {
-          const [_, id] = operation[X_LEGACY_DOC_URL_PROPERTY].split("#");
+function prependLegacyUrlAnchor(target) {
+    if (target[X_LEGACY_DOC_URL_PROPERTY]) {
+        const [_, id] = target[X_LEGACY_DOC_URL_PROPERTY].split("#");
 
-          if (!id) {
-            console.warn(`Invalid legacy doc URL on ${operation.operationId}: ${operation[X_LEGACY_DOC_URL_PROPERTY]}`);
+        if (!id) {
+            console.warn(`Invalid legacy doc URL on ${target.operationId}: ${target[X_LEGACY_DOC_URL_PROPERTY]}`);
             return;
-          }
-
-          operation.description = `<div id="${id}"></div>${operation.description || ""}`;
         }
-      }
-    },
-  }
+
+        target.description = `<div id="${id}"></div>${target.description || ""}`;
+    }
 }
 
-module.exports = LegacyDocUrlDecorator;
+module.exports = () => ({
+    Tag: {
+        leave: prependLegacyUrlAnchor
+    },
+    Operation: {
+        leave: prependLegacyUrlAnchor
+    },
+});
 
