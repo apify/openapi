41383a9292c94bbfa977e07ce6e455339eba7164
Tobiáš Potoček <tobias.potocek@apify.com>
2024-07-01 12:59:52 +0200

    feat: implement legacy doc URL decorator (#60)
    The decorator prepends an HTML anchor to the description of each
    operation with an HTML fragment from the legacy documentation.
    This achieves link backward compatibility.
    
    The legacy URL is stored on a custom property `x-legacy-doc-url` in the
    operation object.
    
    The PR populates the field in a few places so that it can be tested.

---
+++ plugins/apify.js
@@ -0,0 +1,10 @@
+const LegacyDocUrlDecorator = require("./decorators/legacy-doc-url-decorator");
+
+module.exports = {
+    id: 'apify',
+    decorators: {
+        oas3: {
+            'legacy-doc-url-decorator': LegacyDocUrlDecorator
+        }
+    }
+}
